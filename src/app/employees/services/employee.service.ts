f2e7fd8393693b0e98155d706c2e7b8960f62d95
blessingsithole <blessings154@gmail.com>
2025-11-30 22:30:57 +0200

    update styling and refactor

--- src/app/employees/services/employee.service.ts
+++ src/app/employees/services/employee.service.ts
@@ -13,21 +13,22 @@ export class EmployeeService {
   constructor(private http: HttpClient) { }
 
   createEmployee(employee: User): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}`, employee);}
+    return this.http.post<User>(`${this.apiUrl}`, employee);
+  }
 
-    getAllEmployees(): Observable<User[]> {
-      return this.http.get<User[]>(this.apiUrl);
-    }
+  getAllEmployees(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
 
-    getEmployeeById(id: number): Observable<User> { 
-      return this.http.get<User>(`${this.apiUrl}}/${id}`);
-    }
+  getEmployeeById(id: number): Observable<User> { 
+    return this.http.get<User>(`${this.apiUrl}/${id}`);
+  }
 
-    updateEmployee(employee: User): Observable<User> {
-      return this.http.put<User>(`${this.apiUrl}/${employee.id}`, employee);
-    }
+  updateEmployee(employee: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/${employee.id}`, employee);
+  }
 
-    deleteEmployee(id: number): Observable<void> {
-      return this.http.delete<void>(`${this.apiUrl}/${id}`)
-    }
+  deleteEmployee(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
