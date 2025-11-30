cb1f0cec49dea0e7a9d263e67919a997fb62fdcb
blessingsithole <blessings154@gmail.com>
2025-11-30 22:17:06 +0200

    create employee components

---
+++ src/app/employees/employee-create/employee-create.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+import { Router } from '@angular/router';
+import { User } from '../../shared/models/user';
+import { ReactiveFormsModule } from '@angular/forms'; 
+
+@Component({
+  selector: 'app-employee-create',
+  imports: [ReactiveFormsModule],
+  templateUrl: './employee-create.component.html',
+  styleUrl: './employee-create.component.scss'
+})
+export class EmployeeCreateComponent {
+
+  employeeForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.employeeForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      department: [''],
+      position: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.employeeForm.valid) {
+      const employee: User = this.employeeForm.value;
+
+      this.employeeService.createEmployee(this.employeeForm.value).subscribe({
+        next: () => {
+          alert('Employee created successfully!');
+        this.router.navigate(['/employees']);
+        },
+        error: (err: any) => {
+          console.error('Error creating employee:', err);
+          alert('Failed to create employee. Please try again.');
+        }
+      });
+    }
+  }
+}
