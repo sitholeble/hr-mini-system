a388fa0170dda96b3e364e6e11b4e4f9c288fc80
blessingsithole <blessings154@gmail.com>
2025-11-30 22:26:00 +0200

    configure routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EmployeeCreateComponent } from './employees/employee-create/employee-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+  { path: 'employees', loadComponent: () => import('./employees/employee-list/employee-list.component').then(m => m.EmployeeListComponent) },
+  { path: 'employees/create', component: EmployeeCreateComponent },
+  { path: '**', redirectTo: '/employees' }
+];
